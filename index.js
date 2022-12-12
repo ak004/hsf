f70520adcae26d6066f2b48474ad7f0e1fed67f2
bilalli1 <pilalalimohamed@gmail.com>
2022-12-12 05:12:29 -0800

    1st push

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const cookieSession = require("cookie-session");
 const mongoose = require("mongoose")
 const Api = require('./Routes/api');
 const app = express();
+mongoose.set('strictQuery', true);
+// var corsOptions = {
+//   origin: "http://localhost:8081"
+// };
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
-
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -21,11 +21,11 @@ app.use(bodyParser.json());
 // simple route
 app.use('/api', Api)
 
-const url = "mongodb://localhost:27017/testdb"
+const url = "mongodb://127.0.0.1:27017/HSFdb"
 mongoose.connect(url).then((ans) => {
     console.log("ConnectedSuccessful")
   }).catch((err) => {
-    console.log("Error in the Connection")
+    console.log(err)
   })
 
 
@@ -34,3 +34,4 @@ const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
